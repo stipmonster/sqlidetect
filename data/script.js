c309f73b46ffb8f20053b4fa6b88085030936e00
Erik Kooistra <stipmonster@gmail.com>
2014-01-23 21:36:04 +0100

    added widget and fixed some more url parsing

--- data/script.js
+++ data/script.js
@@ -1,5 +1,4 @@
 self.port.on("display", function (url,score) {
   var div = document.getElementById('data');
-  console.log("GETING DATA");
-  div.innerHTML=url + "SCORE: " + score;
+  div.innerHTML=url + " SCORE: " + score;
 });
