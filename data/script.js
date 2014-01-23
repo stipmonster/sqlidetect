9aba09929c9d66d579f7fb4f64b1a9833327f761
Erik Kooistra <stipmonster@gmail.com>
2014-01-23 21:01:41 +0100

    improved url searching

--- data/script.js
+++ data/script.js
@@ -1,5 +1,5 @@
 self.port.on("display", function (url,score) {
   var div = document.getElementById('data');
   console.log("GETING DATA");
-  alert(url + " SCORE: " + score);
+  div.innerHTML=url + "SCORE: " + score;
 });
